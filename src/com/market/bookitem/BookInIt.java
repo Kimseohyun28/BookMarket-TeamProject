63624420de5c3c94947df517b5be1c0c320eef54
김서현 <rlatjgus041228@gmail.com>
2025-11-29 15:42:17 +0900

    BookInIt를 파일 대신 DB에서 도서 목록을 불러오도록 변경

--- src/com/market/bookitem/BookInIt.java
+++ src/com/market/bookitem/BookInIt.java
@@ -1,81 +1,33 @@
 package com.market.bookitem;
 
-import java.io.*;
+import com.market.dao.BookDAO;
 import java.util.ArrayList;
 
 public class BookInIt {
-	private static ArrayList<Book> mBookList;
-	private static int mTotalBook = 0;
+    private static ArrayList<Book> mBookList;
+    private static int mTotalBook = 0;
 
-	public static void init() {
-		mTotalBook = totalFileToBookList();
-		mBookList = new ArrayList<Book>();
-		setFileToBookList(mBookList);
-	}
+    // 프로그램 시작 시 한 번만 호출해서 DB에서 책 목록 불러오기
+    public static void init() {
+        BookDAO dao = new BookDAO();
+        mBookList = dao.findAll();     // DB에서 전체 도서 목록 조회
+        mTotalBook = mBookList.size(); // 총 도서 개수
+    }
 
-	public static int totalFileToBookList() {
-		try {
-			FileReader fr = new FileReader("book.txt");
-			BufferedReader reader = new BufferedReader(fr);
+    public static ArrayList<Book> getmBookList() {
+        return mBookList;
+    }
 
-			String str;
-			int num = 0;
-			while ((str = reader.readLine()) != null) {
-				if (str.contains("ISBN"))
-					++num;
-			}
-			reader.close();
-			fr.close();
-			return num;
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		return 0;
-	}
+    public static void setmBookList(ArrayList<Book> mBookList) {
+        BookInIt.mBookList = mBookList;
+        BookInIt.mTotalBook = (mBookList != null) ? mBookList.size() : 0;
+    }
 
-	public static void setFileToBookList(ArrayList<Book> booklist) {
-		try {
-			FileReader fr = new FileReader("book.txt");
-			BufferedReader reader = new BufferedReader(fr);
+    public static int getmTotalBook() {
+        return mTotalBook;
+    }
 
-			String str2;
-			String[] readBook = new String[7];
-
-			while ((str2 = reader.readLine()) != null) {
-				if (str2.contains("ISBN")) {
-					readBook[0] = str2;
-					readBook[1] = reader.readLine();
-					readBook[2] = reader.readLine();
-					readBook[3] = reader.readLine();
-					readBook[4] = reader.readLine();
-					readBook[5] = reader.readLine();
-					readBook[6] = reader.readLine();
-				}
-
-				Book bookitem = new Book(readBook[0], readBook[1], Integer.parseInt(readBook[2]), readBook[3],
-						readBook[4], readBook[5], readBook[6]);
-				booklist.add(bookitem);
-			}
-			reader.close();
-			fr.close();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-	}
-
-	public static ArrayList<Book> getmBookList() {
-		return mBookList;
-	}
-
-	public static void setmBookList(ArrayList<Book> mBookList) {
-		BookInIt.mBookList = mBookList;
-	}
-
-	public static int getmTotalBook() {
-		return mTotalBook;
-	}
-
-	public static void setmTotalBook(int mTotalBook) {
-		BookInIt.mTotalBook = mTotalBook;
-	}
+    public static void setmTotalBook(int mTotalBook) {
+        BookInIt.mTotalBook = mTotalBook;
+    }
 }
